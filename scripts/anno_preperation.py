5585895595b6a504dd84634cca9636e0281b199c
FemkeBakker <femke.bakker02@gmail.com>
2023-06-10 21:28:59 +0200

    update

---
+++ scripts/anno_preperation.py
@@ -0,0 +1,18 @@
+# from scripts.filter import filter_and_extract
+# from scripts.sentences_py import load_text, edit_docs
+from filter import filter_and_extract
+from sentences_py import load_text, edit_docs
+
+""" Prepare data before annotation """
+
+# filter pdfs based on image- or textbased, using threshold = 0.1
+# convert pdf into txt files, save files in IMI_txt_0.1
+
+filter_and_extract("data/preparation/IMIpdfs", "data/preparation/txt_files/IMI_txt_0.1", 0.1)
+
+# filter out sentences that do not have date, 
+# save date labels for annotation in data/IMItextclean_0.1
+documents, ids = load_text("data/preparation/txt_files/IMI_txt_0.1/")
+edit_docs(documents, "data/preparation/IMItextclean_0.1", ids)
+
+
---
+++ scripts/remove_mistakes.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def remove_false_dates(df, gt):
+    print(f"Original length of dataframe:{len(df)}")
+    df_copy = df.copy()
+    for index, row in df.iterrows():
+        gt_row = gt.loc[(gt['text'] == row['date']) & (gt['start'] == row['start_in_text']) & (gt['end'] == row['end_in_text']) & (gt['doc_id'] == row['doc_id']) & (gt['sentence']==row['sentence'])]
+        if len(gt_row) > 1:
+            print("error: not unique row in ground truth")
+
+        if len(gt_row) == 0:
+            df_copy = df_copy.drop(row.name)
+   
+
+    print(f"New length after removing mistakes: {len(df_copy)}")
+    return df_copy
+
+
+def remove_no_event_dates(df, gt):
+    print(f"Original length of dataframe:{len(df)}")
+    df_copy = df.copy()
+    for index, row in df.iterrows():
+        gt_row = gt.loc[(gt['text'] == row['date']) & (gt['start'] == row['start_in_text']) & (gt['end'] == row['end_in_text']) & (gt['doc_id'] == row['doc_id']) & (row['sentence']==gt['sentence'])]
+        
+        if len(gt_row) > 1:
+            print("error: not unique row in ground truth")
+
+        if len(gt_row) == 0:
+            # print(row.name)
+            df_copy = df_copy.drop(row.name)
+
+        elif gt_row.iloc[0]['label'] == 'DATE+':
+            df_copy = df_copy.drop(row.name)
+
+   
+
+    print(f"New length after removing mistakes: {len(df_copy)}")
+    return df_copy
